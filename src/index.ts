2af115632af0454ad9179e3faab0fd0fd2d6e1f1
chaenchaen99 <chaenjung.99@gmail.com>
2025-03-10 12:29:55 +0900

    express 기본 세팅

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/hello', (req, res) => {
+  res.send('Hello, TypeScript with Express!');
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
