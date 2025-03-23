584c4c50f418b19b4fb687b21c186399a2b312ca
chaenchaen99 <chaenjung.99@gmail.com>
2025-03-23 21:13:50 +0900

    db mysql 연동 및 prisma 설치

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,20 +1,42 @@
 import { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export const home = (req: Request, res: Response) => {
     res.send('ok');
 };
 
-export const findById = (req: Request, res: Response) => {
-    const { id } = req.params;
-    console.log(id);
-    res.send('유저정보');
+//id로 조회
+export const getUserById = async (req: Request, res: Response) => {
+    const {id} = req.query;
+    const user = await prisma.user.findUnique({
+        where: {id: Number(id)}
+    });
+    res.json(user);
+} 
+
+//회원 전체 조회
+export const getAllUsers = async (req: Request, res: Response) => {
+    try {
+        const users = await prisma.user.findMany();
+        res.json(users);
+    } catch (error) {       
+        res.status(500).json({ error: 'Failed to fetch users' });
+    }
 };
 
-export const createUser = (req: Request, res: Response) => {
-    const { id,pw } = req.body;
-    console.log(id,pw);
-    res.send('ok');
-};
 
+// export const findById = (req: Request, res: Response) => {
+//     const { id } = req.params;
+//     console.log(id);
+//     res.send('유저정보');
+// };
+
+// export const createUser = (req: Request, res: Response) => {
+//     const { id,pw } = req.body;
+//     console.log(id,pw);
+//     res.send('ok');
+// };
 
 
--- src/routers/UserRoute.ts
+++ src/routers/UserRoute.ts
@@ -1,15 +1,22 @@
 import { Router } from "express";
-import { home,findById,createUser } from "../controllers/UserController";
+import { home,getAllUsers, getUserById } from "../controllers/UserController";
+// import { home,findById,createUser } from "../controllers/UserController";
 const router = Router();
 
 //홈페이지
 router.get('/', home);
 
+//회원 전체 조회
+router.get('/users', getAllUsers);
+
 //id로 조회
-router.get('/:id', findById);
+router.get('/getUserById', getUserById);
+
+// //id로 조회
+// router.get('/:id', findById);
  
-//create요청
-router.post('/create', createUser);
+// //create요청
+// router.post('/create', createUser);
 
 //create요청
 // router.post('/create', (req, res) => {
